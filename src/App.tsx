e69ea98dd8fec3309cac1f93acc67911576b9e92
Torjer <andres22jld@gmail.com>
2021-12-28 19:26:50 -0600

    Transpiling, bundling and execution working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
+  const [input, setInput] = useState('');
+  // const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+
+  const handleClick = async () => {
+    if (!ref.current) return;
+
+    // Reset the iframe content
+    iframe.current.srcdoc = html;
+
+    // Bundle and transpile code in the input
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data)
+            } catch(e) {
+              document.querySelector('#root').innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + e + '</div>'
+              console.error(e)
+            }
+          }, false) 
+        </script>
+      </body>
+    </html>
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <iframe
+        ref={iframe}
+        title="code executed"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
-}
+};
 
 export default App;
